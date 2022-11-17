84ef7f00e13d25cd36ea0ab31ed3a71115ee5a0c
Vladyslav Mazurenko <mazur097mazur@gmail.com>
2022-11-17 01:55:57 +0100

    Show a common sphere

--- src/script.js
+++ src/script.js
@@ -3,38 +3,66 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Loading
+const textureLoader = new THREE.TextureLoader()
+
+const coinNormalMap = textureLoader.load('/textures/coinNormalMap.jpeg')
+
 // Debug
 const gui = new dat.GUI()
 
 // Canvas
-const canvas = document.querySelector('.torus')
+const coinCanvas = document.querySelector('.coin__canvas')
 
 // Scene
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry(.7, .2, 16, 100);
+const coinGeometry = new THREE.SphereBufferGeometry(0.5, 64, 64)
 
 // Materials
-
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const coinMaterial = new THREE.MeshStandardMaterial()
+coinMaterial.normalMap = coinNormalMap
+coinMaterial.color = new THREE.Color(0x000000)
 
 // Mesh
-const sphere = new THREE.Mesh(geometry, material)
-scene.add(sphere)
+const coin = new THREE.Mesh(coinGeometry, coinMaterial)
+scene.add(coin)
 
 // Lights
+const bluePointLight = new THREE.PointLight(0x0000ff, 1)
+bluePointLight.position.set(0, 2.5, 0)
+bluePointLight.intensity = 10
+scene.add(bluePointLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const yellowPointLight = new THREE.PointLight(0xffff00, 1)
+yellowPointLight.position.set(0, -2.5, 0)
+yellowPointLight.intensity = 10
+scene.add(yellowPointLight)
 
-/**
- * Sizes
- */
+// GUI
+// . GUI_folders
+const bluePointLightFolder = gui.addFolder('Blue point light')
+const yellowPointLightFolder = gui.addFolder('Yellow point light')
+
+// . GUI_controls
+bluePointLightFolder.add(bluePointLight.position, 'x').min(-10).max(10).step(0.01)
+bluePointLightFolder.add(bluePointLight.position, 'y').min(-10).max(10).step(0.01)
+bluePointLightFolder.add(bluePointLight.position, 'z').min(-10).max(10).step(0.01)
+bluePointLightFolder.add(bluePointLight, 'intensity').min(-10).max(10).step(0.01)
+yellowPointLightFolder.add(yellowPointLight.position, 'x').min(-10).max(10).step(0.01)
+yellowPointLightFolder.add(yellowPointLight.position, 'y').min(-10).max(10).step(0.01)
+yellowPointLightFolder.add(yellowPointLight.position, 'z').min(-10).max(10).step(0.01)
+yellowPointLightFolder.add(yellowPointLight, 'intensity').min(-10).max(10).step(0.01)
+
+// Helpers
+const bluePointLightHelper = new THREE.PointLightHelper(bluePointLight, 1)
+scene.add(bluePointLightHelper)
+
+const yellowPointLightHelper = new THREE.PointLightHelper(yellowPointLight, 1)
+scene.add(yellowPointLightHelper)
+
+// Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
@@ -54,10 +82,7 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
 camera.position.y = 0
@@ -65,22 +90,18 @@ camera.position.z = 2
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
+// const controls = new OrbitControls(camera, coinCanvas)
 // controls.enableDamping = true
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: coinCanvas,
+  alpha: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
-
+// Animate
 const clock = new THREE.Clock()
 
 const tick = () => {
@@ -88,7 +109,7 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime()
 
   // Update objects
-  sphere.rotation.y = .5 * elapsedTime
+  coin.rotation.y = .5 * elapsedTime
 
   // Update Orbital Controls
   // controls.update()
